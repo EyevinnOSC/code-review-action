5f43e603de0dbe7c35f0be93ce347d24467f46dd
Jonas Birmé <jonas.birme@eyevinn.se>
2024-12-13 17:56:16 +0100

    feat: action to code review

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# code-review-action
+# GitHub Action for AI Code Reviewer
+
+This action uses the AI Code Reviewer open web service in Eyevinn Open Source Cloud to perform code reviews.
+
+## Usage
+
+`workflow.yml` Example
+
+```yml
+name: Perform code review
+
+on: [pull_request]
+
+jobs:
+  check:
+    runs-on: ubuntu-latest
+    steps:
+    - name: review
+      uses: EyevinnOSC/code-review-action@v1
+      with:
+        repo_url: ${{ github.server_url }}/${{ github.repository }}/tree/${{ github.ref_name}}
+      env:
+        OSC_ACCESS_TOKEN: ${{ secrets.OSC_ACCESS_TOKEN }}
+    - name: comment
+      uses: actions/github-script@v7
+      with:
+        github-token: ${{secrets.GITHUB_TOKEN}}
+        script: |
+          github.rest.issues.createComment({
+            issue_number: context.issue.number,
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            body: 'Code review score: ${{ steps.review.outputs.score }}'
+          }) 
+```
+
+## Development
+
+```
+% INPUT_REPO_URL=https://github.com/Eyevinn/ai-code-reviewer npm run local-action
+```
---
+++ src/main.ts
@@ -0,0 +1,55 @@
+import * as core from '@actions/core';
+import { Context } from '@osaas/client-core';
+import {
+  createEyevinnAiCodeReviewerInstance,
+  getEyevinnAiCodeReviewerInstance
+} from '@osaas/client-services';
+
+const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+async function reviewCode(gitHubUrl: URL) {
+  core.info('Setting up Code Reviewer');
+  const ctx = new Context();
+  let reviewer = await getEyevinnAiCodeReviewerInstance(ctx, 'ghaction');
+  if (!reviewer) {
+    reviewer = await createEyevinnAiCodeReviewerInstance(ctx, {
+      name: 'ghaction',
+      OpenAiApiKey: '{{secrets.openaikey}}'
+    });
+    await delay(1000);
+  }
+  core.info(`Reviewer available, requesting review of ${gitHubUrl.toString()}`);
+  const reviewRequestUrl = new URL('/api/v1/review', reviewer.url);
+  core.debug(reviewRequestUrl.toString());
+  const sat = await ctx.getServiceAccessToken('eyevinn-ai-code-reviewer');
+  const response = await fetch(reviewRequestUrl, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${sat}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      githubUrl: gitHubUrl.toString()
+    })
+  });
+  if (response.ok) {
+    const review = await response.json();
+    return review;
+  } else {
+    throw new Error('Failed to get review');
+  }
+}
+
+/**
+ * This file is the actual logic of the action
+ * @returns {Promise<void>} Resolves when the action is complete
+ */
+export async function run(): Promise<void> {
+  try {
+    const repoUrl = core.getInput('repo_url', { required: true });
+    const response = await reviewCode(new URL(repoUrl));
+    core.setOutput('score', response.review.scoring_criteria.overall_score);
+  } catch (error: any) {
+    core.setFailed(error.message);
+  }
+}
